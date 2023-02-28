67ea257436bafed6886859a7a6d827d2156e4d4d
ben11997 <abrahambenson90@gmail.com>
2023-02-28 10:50:17 +0100

    Recurse it!

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,30 +1,25 @@
 
-n to query a list of all hot posts on a given Reddit subreddit."""
-import requests
-
-
-def recurse(subreddit, hot_list=[], after="", count=0):
-    """Returns a list of titles of all hot posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        return None
-
-    results = response.json().get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        hot_list.append(c.get("data").get("title"))
-
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_list
+#!/usr/bin/python3
+def recurse(subreddit, hot_list=[]):
+    """
+    Queries the Reddit API and returns a list containing the titles of all 
+    hot articles for a given subreddit. If no results are found for the 
+    given subreddit, the function should return None.
+    """
+    import requests
+    
+    # Set custom User-Agent to avoid Too Many Requests error
+    headers = {'User-Agent': 'Reddit Article Title Checker'}
+    
+    # Make the request
+    response = requests.get('https://www.reddit.com/r/' + subreddit + '/hot.json', headers=headers)
+    
+    # Check if subreddit is valid
+    if response.status_code == 200:
+        # Parse json data
+        data = response.json()
+        # Append titles to hot_list
+        for article in data['data']['children']:
+            hot_list.append(article['data']['title'])
+        # Check if there are more pages
+        if data['data']['after'] is not None:
