785d0008a7e33eea90871c81aabf078747e12b53
ben11997 <abrahambenson90@gmail.com>
2023-02-28 10:27:22 +0100

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
+"""Function to query subscribers on a given Reddit subreddit."""
+import requests
+
+
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers 
-    (not active users, total subscribers) for a given subreddit.
-    If an invalid subreddit is given, the function should return 0.
-    """
-    import requests
-    
-    # Set custom User-Agent to avoid Too Many Requests error
-    headers = {'User-Agent': 'Reddit Subscriber Checker'}
-    
-    # Make the request
-    response = requests.get('https://www.reddit.com/r/' + subreddit + '/about.json', headers=headers)
-    
-    # Check if subreddit is valid
-    if response.status_code == 200:
-        # Parse json data
-        data = response.json()
-        # Return the number of subscribers
-        return data['data']['subscribers']
-    else:
-        # If invalid subreddit, return 0
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
