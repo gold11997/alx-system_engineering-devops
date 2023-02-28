7ef0785ec1ac4cc292232dec4eede5abfea54e2d
ben11997 <abrahambenson90@gmail.com>
2023-02-28 11:03:50 +0100

    Recurse it!

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,25 +1,29 @@
-
 #!/usr/bin/python3
-def recurse(subreddit, hot_list=[]):
-    """
-    Queries the Reddit API and returns a list containing the titles of all 
-    hot articles for a given subreddit. If no results are found for the 
-    given subreddit, the function should return None.
-    """
-    import requests
-    
-    # Set custom User-Agent to avoid Too Many Requests error
-    headers = {'User-Agent': 'Reddit Article Title Checker'}
-    
-    # Make the request
-    response = requests.get('https://www.reddit.com/r/' + subreddit + '/hot.json', headers=headers)
-    
-    # Check if subreddit is valid
-    if response.status_code == 200:
-        # Parse json data
-        data = response.json()
-        # Append titles to hot_list
-        for article in data['data']['children']:
-            hot_list.append(article['data']['title'])
-        # Check if there are more pages
-        if data['data']['after'] is not None:
+""" Queries the Reddit API and returns
+a list containing the titles of all hot articles for a given subreddit."""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """ Queries the Reddit API """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"after": after},
+    )
+
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            hot_list.append(title)
+        after = req.json().get("data").get("after")
+
+        if after is None:
+            return hot_list
+        else:
+            return recurse(subreddit, hot_list, after)
+    else:
+        return None
+
