a7677d26b5dbe241f08a22fe813b386ddc4ece4c
ben11997 <abrahambenson90@gmail.com>
2023-08-31 02:10:37 +0100

    update

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,26 +3,37 @@
 Using reddit's API
 """
 import requests
-after = None
 
 
-def recurse(subreddit, hot_list=[]):
-    """returning top ten post titles recursively"""
-    global after
+def recurse(subreddit, hot_list=[], after=None):
     user_agent = {'User-Agent': 'api_advanced-project'}
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    parameters = {'after': after}
-    results = requests.get(url, params=parameters, headers=user_agent,
-                           allow_redirects=False)
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    params = {'after': after}
 
-    if results.status_code == 200:
-        after_data = results.json().get("data").get("after")
-        if after_data is not None:
-            after = after_data
-            recurse(subreddit, hot_list)
-        all_titles = results.json().get("data").get("children")
-        for title_ in all_titles:
-            hot_list.append(title_.get("data").get("title"))
-        return hot_list
+    response = requests.get(url, params=params,
+                            headers=user_agent, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json().get("data")
+        if data and "children" in data:
+            children = data["children"]
+            if not children:
+                return hot_list
+            for child in children:
+                title = child.get("data", {}).get("title")
+                if title:
+                    hot_list.append(title)
+            after = data.get("after")
+            return recurse(subreddit, hot_list, after)
+    return None
+
+
+if __name__ == "__main__":
+    subreddit_name = "your_subreddit_here"
+    hot_titles = recurse(subreddit_name)
+
+    if hot_titles is None:
+        print("Invalid subreddit or no results found.")
     else:
-        return (None)
+        for i, title in enumerate(hot_titles, start=1):
+            print(f"{i}. {title}")
